65a7d5cda6332739b99ad12ddfa563febd00a896
Nestor Martinez <nestor@alberto.fr>
2019-05-16 12:20:04 +0200

    Pas mal

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,15 @@ class ArticlesController < ApplicationController
 
   def index
     @articles = Article.all
-  end 
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def new
     @article = Article.new
@@ -18,16 +26,26 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice]="Article was successfully updated" #message affiché
+      redirect_to article_path(@article) #redirection vers la page initiale des articles
+    else
+      render 'edit' #sinon recharge la page
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article was successfully deleted"
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
 end
